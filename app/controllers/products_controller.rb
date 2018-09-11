87a66125e03338ceeb144ec3066b1c2866467902
juanraba <jrabanillog@gmail.com>
2018-09-10 21:33:48 -0300

    form with product category show

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,12 @@
 class ProductsController < ApplicationController
+  def create
+    @product = Product.new(product_params)
+    redirect_to category_path if @product.save
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :price)
+  end
 end
